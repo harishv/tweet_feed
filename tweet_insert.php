9ffa8f247dd3990b018878313ea369398c06901e
Harish Varada <harish.varada@gmail.com>
2013-03-24 02:05:26 +0530

    Finished the functionality of fetching top ten tweets.
    Need to write the Datatable code, Login with twitter code yet.

--- tweet_insert.php
+++ tweet_insert.php
@@ -1,45 +1,50 @@
 <?php
-
 define('DB_NAME', 'tweet_feed');    // The name of the database
 define('DB_USER', 'root');     // Your MySQL username
-define('DB_PASSWORD', ''); // …and password
+define('DB_PASSWORD', ''); // and password
 define('DB_HOST', 'localhost');    // 99% chance you won’t need to change this value
+
+include_once 'tweets.php';
 
 //connect to the database
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $username=$_POST['user_name'];
-//retrieve a twitter feed
-$jsonFeed = file_get_contents('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name='.$username.'&count=2&exclude_replies=false');
+
+//retrieve a twitter feed from tweetapi
+$twit_obj = new TweetsApi();
+$jsonFeed = $twit_obj->get_tweets($username);
+
 $json = json_decode($jsonFeed);
 
 $latest_time=$mysqli->query("SELECT created_at FROM Tweets  order by created_at desc limit 1");
 
 $latest_time_result=$latest_time->fetch_assoc();
 if($latest_time_result){
-$latest_tweet=strtotime($latest_time_result['created_at']);
+	$latest_tweet=strtotime($latest_time_result['created_at']);
 }
-else 
-$latest_tweet=0;
+else
+	$latest_tweet=0;
 
+// We use https://api.twitter.com/1/related_results/show/<tweet_str_id>.json?include_entities=1
+// to get the replies of a specific tweet
 
-//input each of the tweets into a database table
+// Even this may help https://dev.twitter.com/docs/api/1/post/statuses/update
+
+// Insert each of the tweets into a database table
 foreach($json as $js){
-
 	if(strtotime($js->created_at) > $latest_tweet){
-	$abc=$mysqli->query("INSERT IGNORE INTO Tweets (tweet_msg,user_id_twitted,user_name_twitted,user_profile_pic,favourites,retweeted,created_at) VALUES
- ('".$mysqli->real_escape_string($js->text)."','".$mysqli->real_escape_string($js->user->id)."', '".$mysqli->real_escape_string($js->user->screen_name)."',
-'".$mysqli->real_escape_string($js->user->profile_image_url)."', '".$mysqli->real_escape_string($js->user->favourites_count)."','".$mysqli->real_escape_string($js->retweeted)."',
- '".$mysqli->real_escape_string(date('Y-m-d H:i:s',strtotime($js->created_at)))."')");
-
-}
+		$abc=$mysqli->query("INSERT IGNORE INTO Tweets (tweet_str_id,tweet_msg,user_id_twitted,user_name_twitted,user_profile_pic,favourites,retweeted,created_at) VALUES ('".$mysqli->real_escape_string($js->id_str)."', '".$mysqli->real_escape_string($js->text)."', '".$mysqli->real_escape_string($js->user->id)."', '".$mysqli->real_escape_string($js->user->screen_name)."', '".$mysqli->real_escape_string($js->user->profile_image_url)."', '".$mysqli->real_escape_string($js->favorite_count)."', '".$mysqli->real_escape_string($js->retweet_count)."', '".$mysqli->real_escape_string(date('Y-m-d H:i:s',strtotime($js->created_at)))."')");
+	}
 }
 
-$latest_tweets=$mysqli->query("SELECT * FROM Tweets  order by created_at desc limit 10");
-$tweet_str="";
-while($latest_tweets_result=$latest_tweets->fetch_assoc())
+$latest_tweets = $mysqli->query("SELECT * FROM Tweets  order by created_at desc limit 10");
+
+$tweets = array();
+
+while($latest_tweets_result = $latest_tweets->fetch_assoc())
 {
-	
-	$tweet_str .="<li><img src='".$latest_tweets_result['user_profile_pic']."'>  ".$latest_tweets_result['tweet_msg']."  ".$latest_tweets_result['user_name_twitted']."  ".$latest_tweets_result['created_at']."</li>";
+	array_push($tweets, $latest_tweets_result);
 }
-echo $tweet_str;
+
+echo json_encode($tweets, JSON_FORCE_OBJECT);
 ?>
--- twitter.php
+++ twitter.php
@@ -1,38 +1,48 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-          <script type="text/javascript"src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-           <!--<script type="text/javascript" src="https://api.twitter.com/1/statuses/user_timeline/HarishVarada.json?callback=twitterCallback2&count=1"></script>-->
-        <script>
-            $('document').ready(function() {
-    
-   load_tweet();
-   $('#refresh').click(function(){
-load_tweet();
-   });
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
+		<script type="text/javascript"src="js/jquery.min.js"></script>
 
-});
+		<script type="text/javascript">
+			$('document').ready(function() {
+				load_tweet();
 
-  function load_tweet(){
-     data={"user_name":'HarishVarada'};
-    url="http://localhost/twitter_db/tweet_insert.php"
-    $.ajax({
+				$('#refresh').click(function() {
+					load_tweet();
+				});
+			});
 
-          url: url,
-          type: "POST",
-          data: data,     
-          success: function(result) {
-            $('#tweets').html(result);
-          }
-      });
-  }
-      
-        </script>
-    </head>
-    <body>
-      <input type="button" id="refresh" name="refresh" value="refresh"/>
-        <ul id="tweets"></ul>
-    </body>
-    </html>
+			function load_tweet() {
+				data = {
+					"user_name": 'HarishVarada'
+				};
+				url = "tweet_insert.php"
+				$.ajax({
+
+					url: url,
+					type: "POST",
+					data: data,
+					success: function(result) {
+						var tweet_obj = jQuery.parseJSON(result);
+						console.log(tweet_obj);
+						alert(Object.keys(tweet_obj).length);
+						$.each(tweet_obj, function(tweet_num, tweet) {
+							$("#tweets").append("#" + tweet_num + " : ");
+							$.each(tweet, function(key, val) {
+								$("#tweets").append(key + ": " + val + "<br>");
+							});
+							$("#tweets").append("<br><br><br>");
+						});
+						// $('#tweets').html(result);
+					}
+				});
+			}
+		</script>
+	</head>
+	<body>
+		<input type="button" id="refresh" name="refresh" value="refresh"/>
+		<ul id="tweets"></ul>
+	</body>
+</html>
